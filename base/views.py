e6d3f4c512f3a7252623dbf7612a7e8ff45e1b51
Sarvotam <hengajusarvottam@gmail.com>
2023-11-27 08:39:22 +0545

    CRUD operation for Task

--- base/views.py
+++ base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+#To create view we need to import
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 # Create your views here.
 
@@ -19,3 +22,20 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+    
+#By default this view looks for a template the model name and then the prefix(task) of underscore _form
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+#By default this view also looks for a template the model name and then the prefix(task) of underscore _form
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
